3e9ed5efd920d3cc1f713d132a75bbe061e9f20d
green3red2000-prog <green3red2000@gmail.com>
2026-08-30 11:06:27 +0900

    stock_research_snapshot.batはGit管理対象外に変更

--- PowerShell/tools/StockResearch/stock_research_snapshot.bat
+++ PowerShell/tools/StockResearch/stock_research_snapshot.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0stock_research_snapshot.ps1" -Code 7314
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0stock_research_snapshot.ps1" -Code 9513
 
 pause
